654a308753fbb30f83c7399368718d245070417f
RaghulRameshTrainer <89928567+RaghulRameshTrainer@users.noreply.github.com>
2022-08-24 16:27:28 +0530

    Database Ops

--- db_ops.py
+++ db_ops.py
@@ -3,7 +3,7 @@ import sqlite3
 dbh=sqlite3.connect('cognixia.db')
 c=dbh.cursor()
 def create_table():
-    c.execute("CREATE TABLE employee( empname TEXT, tech TEXT, exp TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS employee( empname TEXT, tech TEXT, exp TEXT)")
 def load_data():
     #c.execute("INSERT INTO employee VALUES('Raghul Ramesh','Python',17)")
     #c.execute("INSERT INTO employee VALUES('Kathir V','Hadoop',10)")
@@ -12,9 +12,35 @@ def load_data():
     e_exp=input("Enter your experience:")
     c.execute("INSERT INTO employee VALUES(?,?,?)",(e_name,e_tech,e_exp))
     dbh.commit()
-
+def update_data():
+    c.execute("UPDATE employee SET tech='Perl' , exp='12' WHERE empname='Vijay'")
+    dbh.commit()
+    c.close()
+    dbh.close()
+def delete_data():
+    c.execute("DELETE FROM employee WHERE empname='Venkatesh'")
+    dbh.commit()
+    c.close()
+    dbh.close()
+def fetch_data():
+    c.execute("SELECT * FROM employee")
+    """
+    fetchone
+    fetchmany
+    fetchall
+    """
+    #print(c.fetchone())
+    #print(c.fetchmany(5))
+    for row in c.fetchall():
+        if row[0] != '':
+            print(row[0]+' --> '+   row[1] + ' ---> ' + str(row[-1]))
+    # print(c.fetchall())
 #create_table()
-for x in range(5):
-    load_data()
-c.close()
-dbh.close()
+# for x in range(5):
+#     load_data()
+# c.close()
+# dbh.close()
+
+#update_data()
+#delete_data()
+fetch_data()
